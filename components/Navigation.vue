51dc93a50b86cbe1fda7980d220c7cae96d62c2d
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-22 13:40:44 +0200

    add navigation

---
+++ components/Navigation.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const showNav = ref(false);
+const navLinks = ref([
+  {
+    title: 'Home',
+    isActive: true
+  },
+  {
+    title: 'Store',
+    isActive: false
+  },
+  {
+    title: 'Outfits',
+    isActive: false
+  }
+]);
+
+const setShowNav = () => {
+  showNav.value = !showNav.value
+}
+
+const updateActiveNavLink = (id: number) => {
+  navLinks.value.forEach(link => link.isActive = false);
+  navLinks.value[id].isActive = true;
+  showNav.value = false;
+}
+
+</script>
+
+<template>
+  <div @click="setShowNav" class="text-3xl shadow fixed top-2 right-3 w-12 h-12 rounded-full flex items-center justify-center z-50 bg-white lg:hidden">
+    <i class="fa-solid fa-bars" :class="[showNav ? 'hidden' : 'block']"></i>
+    <i class="fa-solid fa-xmark" :class="[!showNav ? 'hidden' : 'block']"></i>
+  </div>
+
+  <nav
+      class="bg-white fixed top-0 left-0 w-full h-screen z-40 flex justify-center flex-col items-center -translate-x-full transition-transform lg:translate-x-0 lg:h-auto lg:flex-row lg:justify-between lg:py-2 lg:px-4"
+      :class="[showNav ? 'translate-x-0' : '-translate-x-full']">
+    <p class="text-2xl">LoCo</p>
+    <ul class="flex items-center justify-center flex-col h-3/4 text-2xl gap-5 font-light lg:flex-row lg:text-lg lg:gap-10 xl:gap-14">
+      <li v-for="(navLink, index) in navLinks" :key="navLink.title" @click="updateActiveNavLink(index)" class="px-4 py-1 rounded-full transition-all cursor-pointer"
+          :class="[navLink.isActive ? 'bg-black text-white' : '']">{{ navLink.title }}
+      </li>
+    </ul>
+    <Button text="Sign In" extra-classes="bg-black text-white  hover:bg-gray-700"/>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
