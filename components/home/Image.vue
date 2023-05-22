b1cf4236489cc4c1ebb3db6592862ed7235d03b1
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-22 13:43:47 +0200

    add main image to hompage

---
+++ components/home/Image.vue
@@ -0,0 +1,8 @@
+<template>
+      <nuxt-img
+        src="../public/woman-nuxt.webp"
+        width="1000"
+        height="1000"
+        class="mx-auto mt-10 max-w-xs lg:max-w-xl lg:w-full lg:mt-20"
+      />
+</template>
