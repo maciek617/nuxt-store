92a4fede67fa74ddb165789f6bb9ac9775196a8f
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-22 13:44:41 +0200

    add single outfit component and outfits components to show available outifits in a craousel on the homepage

---
+++ components/home/Outfit.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="h-[550px] bg-gray-100 rounded-t-md relative group">
+    <div class="px-4 py-2 flex gap-3">
+      <nuxt-img
+          src="../public/outfit.jpg"
+          alt="Product image"
+          quality="80"
+          format="webp"
+          width="96"
+          height="96"
+          class="h-12 w-12 object-cover rounded-full"
+      />
+      <div>
+        <p class="text-xl font-bold">{{ designerName }}</p>
+        <p>{{ designerState }}</p>
+      </div>
+    </div>
+    <div class="relative h-full">
+      <nuxt-img
+          src="../public/outfit.jpg"
+          alt="Product image"
+          quality="80"
+          format="webp"
+          width="1000"
+          height="1000"
+          class="h-full w-full object-cover mt-4"
+      />
+      <div
+          class="w-full h-full absolute top-0 left-0 bg-bg-darken flex items-center justify-center opacity-0 transition-opacity group-hover:opacity-100"
+      >
+        <Button
+            text="Check outfit"
+            extra-classes="bg-black text-white hover:bg-gray-700"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  designerName: String,
+  designerState: String,
+  outfitImageLink: String,
+  designerImageLink: String
+})
+</script>
+
+<style scoped></style>
---
+++ components/home/Outfits.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="mt-16">
+    <div>
+      <p class="text-4xl lg:text-6xl xl:text-7xl">
+        It's great to find <span class="gradient-text">yourself</span>
+      </p>
+      <p class="text-gray-600 max-w-xl pt-1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+        asperiores aut consequuntur beatae? Reiciendis error omnis.
+      </p>
+    </div>
+
+    <Swiper
+      :modules="[SwiperAutoplay, SwiperEffectCoverflow]"
+      :slides-per-view="slidesCount"
+      :loop="true"
+      :effect="'coverflow'"
+      :autoplay="{
+        delay: 8000,
+        disableOnInteraction: true,
+      }"
+      :coverflow-effect="{
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      }"
+    >
+      <SwiperSlide
+        v-for="slide in 10"
+        :key="slide"
+        class="mt-12 rounded-t-md cursor-grab"
+      >
+        <HomeOutfit designer-name="John Kowalski" designer-state="Fashion Designer"/>
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</template>
+
+<script setup lang="ts">
+const slidesCount = ref(1);
+
+const handleResizeWindow = () => {
+  slidesCount.value = window.innerWidth > 1024 ? 3 : 1;
+};
+
+onMounted(() => {
+  handleResizeWindow();
+  window.addEventListener('resize', handleResizeWindow);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResizeWindow);
+});
+
+</script>
+
+<style scoped></style>
