dff0e56400f55fd3e6171db27b750c9dba42fa03
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-22 13:46:08 +0200

    add statistics to homepage to show current users and collection available on the app

---
+++ components/home/Stats.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex gap-10 mt-10 lg:mt-16 xl:mt-20 2xl:mt-32">
+    <div class="border-r pr-5">
+      <h2 class="font-bold text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+        20k+
+      </h2>
+      <p class="text-sm text-gray-600">Collections</p>
+    </div>
+    <div>
+      <h2 class="font-bold text-4xl lg:text-5xl xl:text-6xl 2xl:text-8xl">
+        16k+
+      </h2>
+      <p class="text-sm text-gray-600">Users</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
