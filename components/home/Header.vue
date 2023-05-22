538c75a9a6480eafefa5dc2303d0666d17214256
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-22 13:43:01 +0200

    add header text to homepage

---
+++ components/home/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pt-10 max-w-sm lg:max-w-xl lg:pt-24 2xl:max-w-4xl">
+    <p class="text-gray-600 text-sm 2xl:text-base">
+      Absolutely hot collections 🔥
+    </p>
+    <h1 class="text-4xl font-semibold lg:text-5xl xl:text-6xl 2xl:text-8xl">
+      The Best Place To Find And Buy Amazing
+      <span class="gradient-text">Products</span>
+    </h1>
+    <p class="text-gray-600 text-md pt-2 2xl:text-lg">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, deserunt.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
