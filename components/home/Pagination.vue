9359ae28b5530d25ee9d84667e69b8f427fc9275
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-22 13:45:01 +0200

    add pagination to exclusive products

---
+++ components/home/Pagination.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex items-center justify-center gap-6 mt-24">
+    <div>
+      <i class="fa-solid fa-arrow-left cursor-pointer"></i>
+    </div>
+    <p
+      class="w-8 h-8 rounded-full flex items-center justify-center text-white bg-black"
+    >
+      1
+    </p>
+    <p>2</p>
+    <p>3</p>
+    <p>...</p>
+    <p>21</p>
+    <div>
+      <i class="fa-solid fa-arrow-right cursor-pointer"></i>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
