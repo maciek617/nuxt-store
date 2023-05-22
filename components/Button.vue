51dee5cca68acbb1ae24c1a3833993fe88ae6d9e
Maciej Gosztyła <r3cenzialny@gmail.com>
2023-05-22 13:40:22 +0200

    add main reusable button

---
+++ components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button
+      @click="$emit('handleClick')"
+      class="px-6 py-2 rounded-md cursor-pointer group"
+      :class="extraClasses" text={{text}}>
+    <slot/>
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  extraClasses: String,
+});
+
+defineEmits(['handleClick']);
+</script>
